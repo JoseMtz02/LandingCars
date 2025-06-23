a1e4947f44f3b5ace07cbd41f6ef9bcd9afeb0ce
José Martínez <110182478+JoseMtz02@users.noreply.github.com>
2025-06-23 17:20:56 -0500

    se agrega efecto scroll suave en el header

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -1,8 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Renderer2, ElementRef, Inject, PLATFORM_ID } from '@angular/core';
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { ContactFormComponent } from '../contact-form/contact-form';
 import Swiper from 'swiper';
 import { Navigation, Pagination, Autoplay } from 'swiper/modules';
+import { isPlatformBrowser } from '@angular/common';
 
 @Component({
   selector: 'app-home',
@@ -13,6 +14,12 @@ import { Navigation, Pagination, Autoplay } from 'swiper/modules';
   imports: [ContactFormComponent]
 })
 export class HomeComponent implements OnInit {
+  constructor(
+    private renderer: Renderer2,
+    private el: ElementRef,
+    @Inject(PLATFORM_ID) private platformId: any
+  ) {}
+
   ngOnInit(): void {
     new Swiper('swiper-container', {
       modules: [Navigation, Pagination, Autoplay],
@@ -21,5 +28,16 @@ export class HomeComponent implements OnInit {
       autoplay: { delay: 3000 },
       loop: true
     });
+  }
+
+  // Función para manejar el smooth scroll
+  goTo(sectionId: string) {
+    if (isPlatformBrowser(this.platformId)) {
+      const element = document.getElementById(sectionId);
+      if (element) {
+        const topOfElement = element.offsetTop - 80; // Ajusta según la altura del header
+        window.scroll({ top: topOfElement, behavior: 'smooth' });
+      }
+    }
   }
 }
